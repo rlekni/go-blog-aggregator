0b1d19d58e480792d8f9ee1ceb165e5a42a89ac8
rlekni <ralekn@pm.me>
2025-05-16 14:34:21 +0100

    aggregation command

--- handlers.go
+++ handlers.go
@@ -79,3 +79,12 @@ func handlerGetAllUsers(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerAggregate(s *state, cmd command) error {
+	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return fmt.Errorf("couldn't fetch feed: %w", err)
+	}
+	fmt.Printf("Feed: %+v\n", feed)
+	return nil
+}
--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerDeleteAll)
 	cmds.register("users", handlerGetAllUsers)
+	cmds.register("agg", handlerAggregate)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
