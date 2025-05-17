a5244b2f90a8c83d130784367ef2eb101968c83d
rlekni <ralekn@pm.me>
2025-05-17 20:27:43 +0100

    create posts

--- aggregator.go
+++ aggregator.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rlekni/go-blog-aggregator/internal/database"
 )
 
 func scrapeFeeds(s *state) error {
@@ -21,8 +28,36 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("failed to fetch RSS feed: %w", err)
 	}
 
-	for _, rss := range rssFeed.Channel.Item {
-		fmt.Println(rss.Title)
+	for _, item := range rssFeed.Channel.Item {
+		publishedAt := sql.NullTime{}
+		if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+			publishedAt = sql.NullTime{
+				Time:  t,
+				Valid: true,
+			}
+		}
+
+		_, err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			FeedID:    feed.ID,
+			Title:     item.Title,
+			Description: sql.NullString{
+				String: item.Description,
+				Valid:  true,
+			},
+			Url:         item.Link,
+			PublishedAt: publishedAt,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Printf("Couldn't create post: %v", err)
+			continue
+		}
 	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 	return nil
 }
